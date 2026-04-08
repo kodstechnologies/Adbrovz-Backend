118279c9ecb1d0d368f306f6e1de83f3fb713178
SundareswariPonnuselvam <sundareswari2002@gmail.com>
2026-04-08 10:01:13 +0530

    socket

--- src/socket.js
+++ src/socket.js
@@ -58,7 +58,9 @@ const initSocket = (server) => {
             origin: process.env.CORS_ORIGIN?.split(',') || ['http://localhost:3000'],
             methods: ['GET', 'POST'],
             credentials: true
-        }
+        },
+        pingTimeout: 60000,
+        pingInterval: 25000,
     });
 
     io.on('connection', (socket) => {
