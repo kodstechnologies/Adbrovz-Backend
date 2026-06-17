c8229c9fbe05b44c8cfda36941df8866b711cc9b
SundareswariPonnuselvam <sundareswari2002@gmail.com>
2026-06-17 12:49:43 +0530

    10 mb

--- src/middlewares/cloudinary.middleware.js
+++ src/middlewares/cloudinary.middleware.js
@@ -14,7 +14,7 @@ const upload = multer({
     storage: storage,
     fileFilter: fileFilter,
     limits: {
-        fileSize: 50 * 1024 * 1024 // 50MB limit
+        fileSize: 10 * 1024 * 1024 // 10MB limit
     }
 });
 
